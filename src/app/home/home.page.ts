06aa43a02d89761538985490e6597e7228c01d80
Edison <jhonedisongiraldo99@gmail.com>
2021-02-05 18:31:42 -0500

    holi bb

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Capacitor, Plugins } from "@capacitor/core";
+import { GeolocalizacionService } from '../services/geolocalizacion.service';
+const { Geolocation, Toast } = Plugins;
 
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+//import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,12 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 export class HomePage {
 
-  lat
-  long
+  lat: any;
+  lng: any;
+  watchId: any;
 
-  constructor(private geolocation: Geolocation) {}
-
+  constructor(public ngZone: NgZone, private locationService:GeolocalizacionService) {}
+/*
   enviarAlarma() {
     this.geolocation.getCurrentPosition().then((resp) => {
       this.lat = resp.coords.latitude
@@ -23,5 +27,60 @@ export class HomePage {
      });
 
   }
+  */
 
+  async getMyLocation() {
+    const hasPermission = await this.locationService.checkGPSPermission();
+    if (hasPermission) {
+      if (Capacitor.isNative) {
+        const canUseGPS = await this.locationService.askToTurnOnGPS();
+        this.postGPSPermission(canUseGPS);
+      }
+      else { this.postGPSPermission(true); }
+    }
+    else {
+      const permission = await this.locationService.requestGPSPermission();
+      if (permission === 'CAN_REQUEST' || permission === 'GOT_PERMISSION') {
+        if (Capacitor.isNative) {
+          const canUseGPS = await this.locationService.askToTurnOnGPS();
+          this.postGPSPermission(canUseGPS);
+        }
+        else { this.postGPSPermission(true); }
+      }
+      else {
+        await Toast.show({
+          text: 'User denied location permission'
+        })
+      }
+    }
+  }
+
+  async postGPSPermission(canUseGPS: boolean) {
+    if (canUseGPS) { this.watchPosition(); }
+    else {
+      await Toast.show({
+        text: 'Please turn on GPS to get location'
+      })
+    }
+  }
+
+  async watchPosition() {
+    try {
+      this.watchId = Geolocation.watchPosition({}, (position, err) => {
+        this.ngZone.run(() => {
+          if (err) { console.log('err', err); return; }
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude
+          this.clearWatch();
+        })
+      })
+    }
+    catch (err) { console.log('err', err) }
+  }
+
+  clearWatch() {
+    if (this.watchId != null) {
+      Geolocation.clearWatch({ id: this.watchId });
+    }
+  }
 }
